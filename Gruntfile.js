e72d4367688f73db78d9fa2676863d5d759c5d0a
Matt Zabriskie <mattzabriskie@attask.com>
2014-08-20 17:44:31 -0600

    Adding minified files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,29 @@
 /*global module:false*/
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.initConfig({
         jshint: {
             all: ['Gruntfile.js', 'src/keymap.js']
+        },
+
+        uglify: {
+            main: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: 'keymap.min.map',
+                    beautify: {
+                        'ascii_only': true
+                    }
+                },
+                files: {
+                    'keymap.min.js': ['keymap.js']
+                }
+            }
         }
     });
 
     grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('publish', ['jshint', 'uglify']);
 };
